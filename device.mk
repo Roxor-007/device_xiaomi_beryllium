2cdebff256b046553eed7237113f4d4149c09cb8
danascape <danascape@gmail.com>
2021-10-03 23:03:55 +0530

    X01AD: Build gnss 1.0 vendor hal
    *drop memeparts

--- device.mk
+++ device.mk
@@ -81,6 +81,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
-# PocketMode
-PRODUCT_PACKAGES += \
-    XiaomiPocketMode
