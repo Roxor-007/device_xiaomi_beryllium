f613246a47144a105b61af2b70dcf543582c0a2b
notakbuzz <bistodroid@gmail.com>
2021-10-06 09:52:30 +0000

    beryllium: aosp makefile changes

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -6,12 +6,8 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Dot OS stuff.
-$(call inherit-product, vendor/dot/config/common.mk)
-TARGET_INCLUDE_PIXEL_CHARGER := true
-
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := dot_beryllium
+PRODUCT_NAME := aosp_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
@@ -24,7 +20,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Extra flags
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
