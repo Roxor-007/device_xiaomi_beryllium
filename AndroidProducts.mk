4f3c572099fef2b1bc62c8b065b24ace584482ad
Roxor-007 <koyande72@gmail.com>
2022-12-21 10:10:57 +0000

    aosp init & adapt to silvercore kernel

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryllium.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_beryllium-user \
-    lineage_beryllium-userdebug \
-    lineage_beryllium-eng
+    $(LOCAL_DIR)/aosp_beryllium.mk
